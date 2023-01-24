85c6ff7e2244a12b642f879780cc493c3267f055
scott <18712626+scott789@users.noreply.github.com>
2023-01-23 20:48:53 -0500

    Include the AppBar on each page

--- src/pages/DataPage.js
+++ src/pages/DataPage.js
@@ -1,6 +1,9 @@
+import BasicAppBar from "../components/BasicAppBar";
+
 function DataPage() {
   return (
     <div className="container">
+      <BasicAppBar />
       <div className="d-flex justify-content-center">
         <p>404 Not Found</p>
       </div>
